f23b05b285777e3f252e0696820eb6fc0935c782
Aparna Mohan <aparna.april321@gmail.com>
2018-08-31 13:42:14 +0530

    basic page ui

--- server/src/server.py
+++ server/src/server.py
@@ -125,11 +125,17 @@ def gene_details(name):
                     source
                 }
                 ppi_a{
+                    
                     interactor_b
+                    interactor_b_full
+                    taxid_b
 
                 }
                 ppi_b{
+                    
                     interactor_a
+                    interactor_a_full
+                    taxid_a
                 }
                 drug_target{
                   drug_id
@@ -191,29 +197,174 @@ def gene_details(name):
 @app.route("/drug/<name>")
 def drug(name):
     data = query('''
-        query getDrug ($NAME: String) {
+        query getPPI ($NAME: String) {
+            all_genes: gene {
+                uniprot_id
+            }
             gene (where: {name: {_eq: $NAME}}) {
-                 drug_target{
-                  drug_id
-                  gene_name
-                  drug_name{
-                    drug_name
-                  }
+                uniprot_id
+                ppi_a{
+                    
+                    interactor_b
+                    interactor_b_full
+                    taxid_b
+
                 }
+                ppi_b{
+                    
+                    interactor_a
+                    interactor_a_full
+                    taxid_a
+                }
+
             }
         }
     ''', {'NAME': name})
 
     if data != None:
         if len(data['gene']) == 0:
-            link = {}
+            gene = {}
         else:
-            link = data['gene'][0]['drug_target']
+            gene = data['gene'][0]
     else:
-        link = {}
+        gene = {}
+
+    gene_list = [x['uniprot_id'] for x in data['all_genes']]
+    # for ppi------------------------------
+    # source_uniprot =gene['uniprot_id']
+    # if gene['ppi_a']['taxid_a']!=None:
+    #     source_taxid = gene['ppi_a']['taxid_a']
+    # else:
+    #     source_taxid =gene['ppi_b']['taxid_b']
+    # source_data={ interactor:name, organism:source_taxid ,interactor_full:name}
+
+    link_ppi_a=gene['ppi_a']
+    link_ppi_b=gene['ppi_b']
+    for i in link_ppi_a:
+        i['interactor']=i.pop('interactor_b')
+        i['organism']=i.pop('taxid_b')
+        i['interactor_full']=i.pop('interactor_b_full')
+    for j in link_ppi_b:
+        j['interactor']=j.pop('interactor_a')
+        j['organism']=j.pop('taxid_a')
+        j['interactor_full']=j.pop('interactor_a_full')    
+    link_ppi=link_ppi_a + link_ppi_b
+    link_ppi=[dict(t) for t in {tuple(d.items()) for d in link_ppi}]
+    linksp=[]
+    for i in link_ppi:
+        linksp.append ({'source': {'interactor':name,'organism':'human','interactor_full':name},'target': i})
+    node=link_ppi
+    node.append({'interactor':name,'organism':'human','interactor_full':name})
+    data={"nodes":node,"links":linksp}
+    # for g in gene['ppi_b']:
+    #     link_ppi.append(g["interactor_a"])
+
+    # for g in gene['ppi_a']:
+    #     link_ppi.append(g["interactor_b"])
+
+    # # remove duplicates
+    # link_ppi = list(set(link_ppi))
+
+    # link_ppi = [{'interactor': x} for x in link_ppi]
     
     return render_template(
         'drug.html',
-        link=link,
-        name=name
+        gene=gene,
+        name=name,
+        link_ppi=link_ppi,
+        gene_list= gene_list,
+        linksp=linksp,
+        node=node,
+        data=data,
+
+    )
+# mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+@app.route("/ppi/<name>")
+def ppi(name):
+    data = query('''
+        query getPPI ($NAME: String) {
+            all_genes: gene {
+                uniprot_id
+            }
+            gene (where: {name: {_eq: $NAME}}) {
+                uniprot_id
+                ppi_a{
+                    
+                    interactor_b
+                    interactor_b_full
+                    taxid_b
+
+                }
+                ppi_b{
+                    
+                    interactor_a
+                    interactor_a_full
+                    taxid_a
+                }
+
+            }
+        }
+    ''', {'NAME': name})
+
+    if data != None:
+        if len(data['gene']) == 0:
+            gene = {}
+        else:
+            gene = data['gene'][0]
+    else:
+        gene = {}
+
+    gene_list = [x['uniprot_id'] for x in data['all_genes']]
+    # for ppi------------------------------
+    # source_uniprot =gene['uniprot_id']
+    # if gene['ppi_a']['taxid_a']!=None:
+    #     source_taxid = gene['ppi_a']['taxid_a']
+    # else:
+    #     source_taxid =gene['ppi_b']['taxid_b']
+    # source_data={ interactor:name, organism:source_taxid ,interactor_full:name}
+
+    link_ppi_a=gene['ppi_a']
+    link_ppi_b=gene['ppi_b']
+    for i in link_ppi_a:
+        i['interactor']=i.pop('interactor_b')
+        i['organism']=i.pop('taxid_b')
+        i['interactor_full']=i.pop('interactor_b_full')
+    for j in link_ppi_b:
+        j['interactor']=j.pop('interactor_a')
+        j['organism']=j.pop('taxid_a')
+        j['interactor_full']=j.pop('interactor_a_full')    
+    link_ppi=link_ppi_a + link_ppi_b
+    link_ppi=[dict(t) for t in {tuple(d.items()) for d in link_ppi}]
+    
+    linksp=[]
+    for i in link_ppi:
+        linksp.append ({'source': {'interactor':name,'organism':'human','interactor_full':name},'target': i})
+    
+    node=link_ppi
+    source_node={'interactor':name,'organism':'human','interactor_full':name}
+    node.append({'interactor':name,'organism':'human','interactor_full':name})
+    data={"nodes":node,"links":linksp}
+    # for g in gene['ppi_b']:
+    #     link_ppi.append(g["interactor_a"])
+
+    # for g in gene['ppi_a']:
+    #     link_ppi.append(g["interactor_b"])
+
+    # # remove duplicates
+    # link_ppi = list(set(link_ppi))
+
+    # link_ppi = [{'interactor': x} for x in link_ppi]
+    
+    return render_template(
+        'ppi.html',
+        gene=gene,
+        name=name,
+        link_ppi=link_ppi,
+        gene_list= gene_list,
+        linksp=linksp,
+        node=node,
+        source_node=source_node,
+        data=data,
+
     )
