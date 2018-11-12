b90d546a5221cc89bf9a324eb544a525a6fdb55a
Shahidh K Muhammed <shahidh@hasura.io>
2018-11-12 15:03:39 +0530

    add HASURA_URL

--- server/src/hasura.py
+++ server/src/hasura.py
@@ -1,7 +1,8 @@
 import requests
 import json
+import os
 
-HASURA_URL = 'http://raven:8080/v1alpha1/graphql'
+HASURA_URL = os.getenv('HASURA_URL', 'http://raven:8080/v1alpha1/graphql')
 
 def query(q, v=None):
     payload = {
@@ -9,16 +10,14 @@ def query(q, v=None):
         "variables": v
     }
 
-    print("===============================================")
-    print("query: ", q)
-    print("variables: ", v)
     r = requests.post(HASURA_URL, data=json.dumps(payload).encode('utf-8'))
     if r.status_code != 200:
+        print("===============================================")
+        print("query: ", q)
+        print("variables: ", v)
         print("request failed: ", r.status_code)
         print("response text: ", r.text)
         print("===============================================")
         return None
     else:
-        print("response code: ", r.status_code)
-        print("===============================================")
         return r.json()['data']
